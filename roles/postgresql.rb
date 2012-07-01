f519cc3c3692cd82e58a7e2558174e8f983ce55b
Lukasz Kaniowski <lukasz.kaniowski@gmail.com>
2012-07-01 04:47:22 +0200

    postgresql that allows connection from host machine

---
+++ roles/postgresql.rb
@@ -0,0 +1,20 @@
+name "pgsql"
+description "Creates postgresql db"
+
+run_list "apt", "postgresql", "postgresql::server"
+
+# postgres admin user = 'postgres'
+
+override_attributes(
+  :postgresql => {
+    :version => "9.1",
+    :listen_addresses => "*",
+    :hba => [
+      {:method => "trust", :address => "0.0.0.0/0"},
+      {:method => "trust", :address => "::1/0"},
+    ],
+    :password => {
+      :postgres => "postgres123"
+    }
+  }
+)
